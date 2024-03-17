d920fb433825ebe5ad911a9e0468993b9c62ea1d
Matt Rubel <mrubel92@gmail.com>
2024-03-17 19:56:48 -0400

    initial EMR infrastructure

---
+++ infrastructure/winners-processing-cluster/s3.tf
@@ -0,0 +1,27 @@
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket_prefix = "${var.CLUSTER_NAME}-"
+
+  # Allow deletion of non-empty bucket
+  # Example usage only - not recommended for production
+  force_destroy = true
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+}
