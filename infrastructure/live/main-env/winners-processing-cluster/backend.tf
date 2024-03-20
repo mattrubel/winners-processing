dcf017c0e1eacf6e7fd875162ca842308b1bba6f
Matt Rubel <mrubel92@gmail.com>
2024-03-19 21:43:26 -0400

    delete emr cluster

--- infrastructure/live/main-env/winners-processing-cluster/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-597426459950"
-    key            = "winners-processing/winners-processing-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-state-lock"
-  }
-}
--- infrastructure/live/main-env/winners-processing-cluster/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = var.AWS_REGION
-}
-
-module "winners-processing" {
-  source = "../../../winners-processing-cluster"
-}
